6c75194bdd62387cc65e8cdfd8118cf7156f64de
Peter Burkholder <peterb@getchef.com>
2014-11-04 14:40:20 -0500

    Rakefile and chefdk recipe

---
+++ recipes/chefdk.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+#
+# Cookbook Name:: tci
+# Recipe:: chefdk
+#
+# Copyright (c) 2014 The Authors, All Rights Reserved.
+
+
+chef_dk 'chef_dk' do
+  version node['tci']['chef_dk']['version']
+  global_shell_init true
+  action :install
+end
